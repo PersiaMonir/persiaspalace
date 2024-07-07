30ccb71ae2713d27cc8efa0074a2ca8810f757e0
Alkarim Jiwa <ajiwa10@my.bcit.ca>
2024-07-06 20:04:37 -0700

    Final commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,7 +18,7 @@ const Hero = () => {
 	return (
 		<section className='py-12 xl:py-24 h-[auto] bg-bottom'>
 			<div className='container mx-auto'>
-				<div className='flex justify-between gap-x-8'>
+				<div className='flex justify-between md:gap-x-8'>
 					{/* Left Side Content */}
 					<div className='flex max-w-[800px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left'>
 						<Fade
